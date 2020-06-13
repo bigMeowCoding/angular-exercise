b43bf1347c99aa7c5311ff0a7192dabac4e1b4c8
zhouyijun <zhouyijun@ruijie.com.cn>
2020-06-13 17:55:26 +0800

    first work over

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { VirtualScrollComponent } from './virtual-scroll/virtual-scroll.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VirtualScrollComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule
